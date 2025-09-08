0b9a28b7f7800384fbbbde5d0a9b3a02f533dd51
Varsha7330 <168283899+Varsha7330@users.noreply.github.com>
2025-09-09 00:23:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ async function initializeDefaultEvents() {
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://sportshub-system.netlify.app/'
+        'https://sportshub-system.netlify.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
